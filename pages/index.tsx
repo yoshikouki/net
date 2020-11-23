b8bf0ec9b269735f150950ff017d75d7930442a6
yoshikouki <yoshikouki@pepabo.com>
2020-11-23 23:38:44 +0900

    ホーム画面を整理

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,55 +11,34 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://yoshikouki.net">Yoshikouki.net!!1</a>
+          <a href="https://yoshikouki.net">Yoshikouki.net</a>!!1
         </h1>
 
         <p className={styles.description}>
-          Updated at
-          <code className={styles.code}>
-            2020-11-22 11:41:00
-          </code>
+          実験場
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/blog" className={styles.card}>
+            <h3>Blog &rarr;</h3>
+            <p>アウトプットまとめ</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://twitter.com/k2_yoshikouki" className={styles.card}>
+            <h3>Twitter &rarr;</h3>
+            <p>日常系ツイート多め</p>
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://twitter.com/k2_yoshikouki"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          @k2_yoshikouki
         </a>
       </footer>
     </div>
