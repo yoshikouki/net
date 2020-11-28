979e710cd3f3a8af2115593e6a6d416b6a344bf9
yoshikouki <yoshikouki@pepabo.com>
2020-11-28 12:14:05 +0900

    あれ、ディレクトリがおかしいかも

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,7 @@ module.exports = {
       ssh_options : ["StrictHostKeyChecking=no", "PasswordAuthentication=no"],
       ref  : 'origin/main',
       repo : 'git@github.com:yoshikouki/yoshikouki.net.git',
-      path : '~/yoshikouki.net',
+      path : '/home/ec2-user/yoshikouki.net',
       'pre-setup' : 'which git > /dev/null || sudo yum install git',
       'post-deploy' : 'npm install && npm install -g pm2 && pm2 startOrReload ecosystem.config.js --env production',
       env : {
