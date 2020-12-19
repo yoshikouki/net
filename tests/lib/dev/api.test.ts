bf62472c8a9b91e2d7dcd6403d6ff607c455ce57
yoshikouki <yoshikouki@pepabo.com>
2020-12-19 16:05:42 +0900

    APIモジュールのテスト土台を作った

--- tests/lib/dev/api.test.ts
+++ tests/lib/dev/api.test.ts
@@ -1,7 +1,13 @@
-import { returnTest } from "../../../lib/dev/api"
+import * as api from "../../../lib/dev/api"
+
+test('郵便番号で住所を取得する', () => {
+  let res: api.zipCloudJson = api.callApi()
+  expect(res.status).toBe(200)
+  expect(res.results[0]["zipcode"]).toBe("8900073")
+})
 
 test('最初のテスト', () => {
   let res: string;
-  res = returnTest()
+  res = api.returnTest()
   expect(res).toBe("test")
 })
