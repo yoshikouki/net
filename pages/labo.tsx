91136a0bfaedea8984352ab77ebaf6a936439cd8
yoshikouki <yoshikouki@pepabo.com>
2020-12-20 23:49:05 +0900

    APIモジュール使って Next に表示させることはできた

--- pages/labo.tsx
+++ pages/labo.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.scss'
 import Layout from "../components/layout";
+import DevApi from "../lib/dev/api";
 
-export default function Labo() {
-  const addressData = "addressData"
+export default function Labo({addressData}) {
+  const result = addressData.results[0]
+  let address = result.address1 + result.address2 + result.address3
 
   return (
     <Layout>
@@ -16,11 +18,27 @@ export default function Labo() {
         <h1>ラボ</h1>
         <ul>
           <li>
-            {addressData}
+            {addressData.results[0].zipcode}
           </li>
-          <li>test</li>
+          <li>
+            {address}
+          </li>
+          <li>
+            {addressData.status}
+          </li>
         </ul>
       </main>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  let api = new DevApi()
+  const addressData = await api.getAddressData(8900073)
+
+  return {
+    props: {
+      addressData
+    }
+  }
 }
