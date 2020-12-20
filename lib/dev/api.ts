df833d59e9967ac92961b82f6a0d68e328f47f6e
yoshikouki <yoshikouki@pepabo.com>
2020-12-20 17:51:32 +0900

    開発用メソッドを API クラスとして定義し直した

--- lib/dev/api.ts
+++ lib/dev/api.ts
@@ -17,19 +17,21 @@ export interface zipCloudJson {
   status: number
 }
 
-export function callApi(zipcode: number) {
-  let url = 'http://zipcloud.ibsnet.co.jp/api/search'
-  return Axios.get(url, { params : { zipcode : zipcode } })
-    .then((res: AxiosResponse) => {
-      const json: zipCloudJson = res.data
-      return json
-    })
-    .catch(() => {
-      console.log("[ERROR] Axios get")
-      return false
-    })
-}
+export default class Api {
+  public getAddressData(zipcode: number) {
+    let url = 'http://zipcloud.ibsnet.co.jp/api/search'
+    return Axios.get(url, { params : { zipcode : zipcode } })
+      .then((res: AxiosResponse) => {
+        const json: zipCloudJson = res.data
+        return json
+      })
+      .catch(() => {
+        console.log("[ERROR] Axios get")
+        return false
+      })
+  }
 
-export function returnTest() {
-  return "test"
+  public static returnTest() {
+    return "test"
+  }
 }
--- tests/lib/dev/api.test.ts
+++ tests/lib/dev/api.test.ts
@@ -1,14 +1,17 @@
-import * as api from "../../../lib/dev/api"
+import Api from "../../../lib/dev/api";
 
-test('郵便番号で住所を取得する', async () => {
-  const res = await api.callApi(8900073)
-  expect(res.status).toBe(200)
-  expect(res.results[0].address3).toBe("宇宿")
-  expect(res.results[0].zipcode).toBe("8900073")
-})
+describe("開発中の API クラス", () => {
+  test('郵便番号で住所を取得する', async () => {
+    const api = new Api()
+    const res = await api.getAddressData(8900073)
+    expect(res.status).toBe(200)
+    expect(res.results[0].address3).toBe("宇宿")
+    expect(res.results[0].zipcode).toBe("8900073")
+  })
 
-test('最初のテスト', () => {
-  let res: string;
-  res = api.returnTest()
-  expect(res).toBe("test")
+  test('最初のテスト', () => {
+    let res: string;
+    res = Api.returnTest()
+    expect(res).toBe("test")
+  })
 })
