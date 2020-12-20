1eada09527dcd46d3a81955e9730eb1e3ce20c48
yoshikouki <yoshikouki@pepabo.com>
2020-12-20 22:29:25 +0900

    Axios のメソッド切り出しに適応

--- lib/dev/api.ts
+++ lib/dev/api.ts
@@ -8,15 +8,9 @@ export default class DevApi {
   }
 
   public getAddressData(zipcode: number) {
-    let url = 'http://zipcloud.ibsnet.co.jp/api/search'
-    return Axios.get(url, { params : { zipcode : zipcode } })
-      .then((res: AxiosResponse) => {
-        return res.data
-      })
-      .catch(() => {
-        console.log("[ERROR] Axios get")
-        return false
-      })
+    const url = 'http://zipcloud.ibsnet.co.jp/api/search'
+    const params = { zipcode : zipcode }
+    return this.requestGet(url, params)
   }
 
   public requestGet(url: string, params?: {}) {
