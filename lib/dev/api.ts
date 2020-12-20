3443a706c13946a5daa86daa1e0161cb33276fbc
yoshikouki <yoshikouki@pepabo.com>
2020-12-20 22:27:16 +0900

    Note からポストを取得するメソッドを追加

--- lib/dev/api.ts
+++ lib/dev/api.ts
@@ -1,6 +1,12 @@
 import Axios, {AxiosResponse} from 'axios'
 
 export default class DevApi {
+  public getNotePosts() {
+    const url = "https://note.com/api/v2/creators/yoshikouki/contents"
+    const params = { kind : "note" }
+    return this.requestGet(url, params)
+  }
+
   public getAddressData(zipcode: number) {
     let url = 'http://zipcloud.ibsnet.co.jp/api/search'
     return Axios.get(url, { params : { zipcode : zipcode } })
@@ -9,6 +15,17 @@ export default class DevApi {
       })
       .catch(() => {
         console.log("[ERROR] Axios get")
+        return false
+      })
+  }
+
+  public requestGet(url: string, params?: {}) {
+    return Axios.get(url, { params : params })
+      .then((res: AxiosResponse) => {
+        return res.data
+      })
+      .catch(() => {
+        console.log("[ERROR] Axios get (DevApi#requestGet)")
         return false
       })
   }
