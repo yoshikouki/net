e1ff05fc490cdc66b0f0606796a6311bcd047051
yoshikouki <yoshikouki@pepabo.com>
2020-12-20 16:59:10 +0900

    Axios インポートの記述を変更した

--- lib/dev/api.ts
+++ lib/dev/api.ts
@@ -1,4 +1,4 @@
-import * as axios from 'axios'
+import Axios, {AxiosResponse} from 'axios'
 
 export interface zipCloudJson {
   message: null,
@@ -19,9 +19,8 @@ export interface zipCloudJson {
 
 export function callApi(zipcode: number) {
   let url = 'http://zipcloud.ibsnet.co.jp/api/search'
-  return axios.default
-    .get(url, { params : { zipcode : zipcode } })
-    .then((res: axios.AxiosResponse) => {
+  return Axios.get(url, { params : { zipcode : zipcode } })
+    .then((res: AxiosResponse) => {
       const json: zipCloudJson = res.data
       return json
     })
