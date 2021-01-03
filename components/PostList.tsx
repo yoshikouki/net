bbd098f94c9603cd771af5dbc776d7a96ed9f060
yoshikouki <yoshikouki@pepabo.com>
2021-01-03 19:02:49 +0900

    Note の投稿をリスト表示させた

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import {BlogPostData, GetBlogPostsResponse} from "../lib/api";
+import {ColDef, DataGrid, RowsProp} from "@material-ui/data-grid";
+import {useRouter} from "next/router";
 
 interface Props {
   posts: GetBlogPostsResponse
 }
 
 export const PostList = ({posts}: Props) => {
-  const title = posts.posts.map((post: BlogPostData) => { return post.title }).join(' ')
+  const router = useRouter()
+  const rows: RowsProp = posts.posts.map((post: BlogPostData) => {
+    return {
+      id: post.url,
+      title: post.title,
+      url: post.url
+    }
+  })
+  const columns: ColDef[] = [
+    { field: 'id', headerName: 'ID', hide: true },
+    { field: 'title', headerName: 'タイトル', width: 600 },
+  ]
+
   return (
-    <p>{title}</p>
+    <div style={{ height: 400, width: 600 }}>
+      <DataGrid
+        columns={columns}
+        rows={rows}
+        pageSize={10}
+        onRowClick={async (param) => {
+          await router.push(rows[param.rowIndex].url)
+        }}
+      />
+    </div>
   )
 }
