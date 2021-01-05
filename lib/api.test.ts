f4251bf8faab8fdae1a8edcd8a3eb2298ad5af0a
yoshikouki <yoshikouki@pepabo.com>
2021-01-06 00:00:37 +0900

    テストが外部APIを叩いたままだったので一旦コメントアウト

--- lib/api.test.ts
+++ lib/api.test.ts
@@ -1,9 +1,9 @@
 import Api from './api'
 
 describe('API クラスのテスト', () => {
-  test('投稿したブログを取得する', async () => {
-    const api = new Api()
-    const posts = await api.getBlogPosts()
-    expect(posts).toBe(Api.ErrorGetBlogPosts)
-  })
+  // test('投稿したブログを取得する', async () => {
+  //   const api = new Api()
+  //   const posts = await api.getBlogPosts()
+  //   expect(posts).toBe(Api.ErrorGetBlogPosts)
+  // })
 })
