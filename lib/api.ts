363dc6a71023f890eb49491b9dee84186d57219e
yoshikouki <yoshikouki@pepabo.com>
2021-01-22 20:42:29 +0900

    note RSS を使用した Api#getBlogPosts を実装

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,44 @@
-import Axios, {AxiosResponse} from "axios";
+import Parser from "rss-parser";
+
+export interface FeedItem {
+  title: string
+  url: string
+  date: number
+  service: string
+}
+
+const ParserCustomField = {
+  customFields: {
+  }
+}
 
 export default class Api {
+  public async getBlogPosts() {
+    const url = 'https://note.com/yoshikouki/rss'
+    const parser = new Parser(ParserCustomField)
+    const feed = await parser.parseURL(url)
+    const items = feed.items.map((item) => {
+      let itemTitle = String(item.title)
+      let itemUrl = String(item.link)
+      let itemDate = new Date(String(item.pubDate)).getTime()
+      const feedItem: FeedItem = {
+        title: itemTitle,
+        url: itemUrl,
+        date: itemDate,
+        service: 'note',
+      }
+      return feedItem
+    })
+    const posts = {
+      posts: items,
+      message: null,
+      status: 200
+    }
+    return posts
+  }
+
+  public static convertDateToString(time: string) {
+    const date = new Date(time)
+    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
+  }
 }
