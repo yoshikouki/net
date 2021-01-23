aa0eec940d474471b7d66bb05f8d5716cf588af9
yoshikouki <yoshikouki@pepabo.com>
2021-01-23 16:31:16 +0900

    備忘録がてら取得してパースしたデータの型定義を追加

--- lib/blog.d.ts
+++ lib/blog.d.ts
@@ -1,7 +1,7 @@
 declare module 'blog' {
   interface BlogPosts {
-    message: null,
-    posts: Post[],
+    message: null
+    posts: Post[]
     status: number
   }
 
@@ -10,5 +10,50 @@ declare module 'blog' {
     url: string
     date: number
     service: string
+  }
+
+  interface NoteFeed {
+    copyright: string
+    description: string
+    feedUrl: string
+    items: NoteFeedItem[]
+    language: string
+    // サンプル "Sat, 23 Jan 2021 16:20:27 +0900"
+    lastBuildDate: string
+    link: string
+    title: string
+  }
+
+  interface NoteFeedItem  {
+    content: string
+    contentSnippet: string
+    guid: string
+    // サンプル "2021-01-19T13:36:17.000Z"
+    isoDate: string
+    link: string
+    // サンプル "Tue, 19 Jan 2021 22:36:17 +0900"
+    pubDate: string
+    title: string
+  }
+
+  interface QiitaFeed {
+    feedUrl: string
+    items: QiitaFeedItem[]
+    // サンプル "2020-11-26T09:36:04.000Z"
+    lastBuildDate: string
+    link: string
+    title: string
+  }
+
+  interface QiitaFeedItem {
+    author: string
+    content: string
+    id: string
+    // サンプル "2020-11-26T09:36:04.000Z"
+    isoDate: string
+    link: string
+    // サンプル "2020-11-26T09:36:04.000Z"
+    pubDate: string
+    title: string
   }
 }
