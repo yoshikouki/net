7dd6c1b59e4aef4f5b789ab768ea1742c174e8a5
yoshikouki <yoshikouki@pepabo.com>
2021-01-30 16:46:40 +0900

    確認用テスト

--- lib/api.test.ts
+++ lib/api.test.ts
@@ -1,4 +1,15 @@
 import Api from './api'
 
 describe('API クラスのテスト', () => {
+  // test('#getBlogPosts',  async () => {
+  //   const api = new Api()
+  //   const posts = await api.getBlogPosts()
+  //   expect(posts.posts.slice(-1)).toBe([])
+  // })
+
+  // test('#fetchRssFeed', async () => {
+  //   const feed = await api.fetchRssFeed('https://qiita.com/yoshikouki/feed.atom')
+  //   const feed = await api.fetchRssFeed('https://note.com/yoshikouki/rss')
+  //   expect(feed.items[0].pubDate).toBe("")
+  // })
 })
